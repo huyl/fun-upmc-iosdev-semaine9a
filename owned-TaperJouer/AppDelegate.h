97d398fdc0cd3e3ab85e5c0a471fb814613b9b0e
huyl <huyl@users.noreply.github.com>
2014-07-04 19:01:25 -0700

    Initial Commit

---
+++ owned-TaperJouer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  owned-TaperJouer
+//
+//  Created by Huy on 7/4/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ owned-TaperJouer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  owned-TaperJouer
+//
+//  Created by Huy on 7/4/14.
+//  Copyright (c) 2014 huy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
